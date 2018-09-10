9a940ef50dae599c8f630947ca2a903da32961b9
Ameya Joshi <ameya.woxi@gmail.com>
2018-09-10 16:44:40 +0530

    Webview changes json

--- app/Http/Controllers/Master/WebviewController.php
+++ app/Http/Controllers/Master/WebviewController.php
@@ -10,26 +10,26 @@ class WebviewController extends Controller
 
 	public function healthPlus() {
 	    $data = "<html><h1>SGKS - Health plus</h1></html>";
-        return $data;
+        return response()->json($data);
     }
 
     public function privacyPolicy() {
 	    $data = "<html><h1>SGKS - Privacy Policy</h1></html>";
-        return $data;
+        return response()->json($data);
     }
 
     public function help() {
 	    $data = "<html><h1>SGKS - Help</h1></html>";
-        return $data;
+        return response()->json($data);
     }
 
     public function qa() {
 	    $data = "<html><h1>SGKS - Q and A</h1></html>";
-        return $data;
+        return response()->json($data);
     }
 
     public function contactUs() {
 	    $data = "<html><h1>SGKS - Contact Us</h1></html>";
-        return $data;
+        return response()->json($data);
     }
 }
