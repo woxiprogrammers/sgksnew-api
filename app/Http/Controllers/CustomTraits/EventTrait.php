ac760a3d468fee2ff194b94e22e4ec255345a5ca
vaibhavkale-woxi <vaibhav.woxi@gmail.com>
2018-11-21 11:31:02 +0530

    here api for event-listing has done

--- app/Http/Controllers/CustomTraits/EventTrait.php
+++ app/Http/Controllers/CustomTraits/EventTrait.php
@@ -1,6 +1,10 @@
 <?php
 namespace App\Http\Controllers\CustomTraits;
 
+use App\Events;
+use App\EventsTranslations;
+use App\EventImages;
+use App\Cities;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 
@@ -8,178 +12,91 @@ trait EventTrait{
 
     public function listing(Request $request){
          try{
-             $displayLength = 5;
-             $totalRecords = $request->page_id * $displayLength;
-             $page_id = 1;
-             $data = array(
-                       array(
-                           'id'=> 1,
-                            "event_name" => "hello  Event One",
-                            "desc" => "hi hello",
-                            "event_date"=> "29th feb 2016 to 30th feb 2017",
-                            "year"=> "2017",
-                            "event_images" => array(
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvg1kO4EfJf6kyCiDeA-PRp-XNwg8DqsucY_OrkHjXqKd2HB2p",
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAhcP9INehHWSK8ML7ONJaWefGjtrObCdQw3DbXiuZ016TRUw7"
-                            )
-                        ),array(
-                           'id'=> 2,
-                            "event_name" => "hello  Event Two",
-                            "desc" => "hi hello",
-                            "event_date"=> "29th feb 2016 to 30th feb 2017",
-                            "year"=> "2017",
-                            "event_images" => array(
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvg1kO4EfJf6kyCiDeA-PRp-XNwg8DqsucY_OrkHjXqKd2HB2p",
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAhcP9INehHWSK8ML7ONJaWefGjtrObCdQw3DbXiuZ016TRUw7"
-                            )
-                        ),array(
-                           'id'=> 3,
-                            "event_name" => "hello  Event Three",
-                            "desc" => "hi hello",
-                            "event_date"=> "29th feb 2016 to 30th feb 2017",
-                            "year"=> "2017",
-                            "event_images" => array(
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvg1kO4EfJf6kyCiDeA-PRp-XNwg8DqsucY_OrkHjXqKd2HB2p",
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAhcP9INehHWSK8ML7ONJaWefGjtrObCdQw3DbXiuZ016TRUw7"
-                            )
-                        ),array(
-                           'id'=> 4,
-                            "event_name" => "hello  Event Four",
-                            "desc" => "hi hello",
-                            "event_date"=> "29th feb 2016 to 30th feb 2017",
-                            "year"=> "2017",
-                            "event_images" => array(
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvg1kO4EfJf6kyCiDeA-PRp-XNwg8DqsucY_OrkHjXqKd2HB2p",
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAhcP9INehHWSK8ML7ONJaWefGjtrObCdQw3DbXiuZ016TRUw7"
-                            )
-                        ),array(
-                           'id'=> 5,
-                            "event_name" => "hello  Event Five",
-                            "desc" => "hi hello",
-                            "event_date"=> "29th feb 2016 to 30th feb 2017",
-                            "year"=> "2017",
-                            "event_images" => array(
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvg1kO4EfJf6kyCiDeA-PRp-XNwg8DqsucY_OrkHjXqKd2HB2p",
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAhcP9INehHWSK8ML7ONJaWefGjtrObCdQw3DbXiuZ016TRUw7"
-                            )
-                        ),array(
-                           'id'=> 6,
-                            "event_name" => "hello  Event Six",
-                            "desc" => "hi hello",
-                            "event_date"=> "29th feb 2016 to 30th feb 2017",
-                            "year"=> "2017",
-                            "event_images" => array(
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvg1kO4EfJf6kyCiDeA-PRp-XNwg8DqsucY_OrkHjXqKd2HB2p",
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAhcP9INehHWSK8ML7ONJaWefGjtrObCdQw3DbXiuZ016TRUw7"
-                            )
-                        ),array(
-                           'id'=> 7,
-                            "event_name" => "hello  Event Seven",
-                            "desc" => "hi hello",
-                            "event_date"=> "29th feb 2016 to 30th feb 2017",
-                            "year"=> "2017",
-                            "event_images" => array(
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvg1kO4EfJf6kyCiDeA-PRp-XNwg8DqsucY_OrkHjXqKd2HB2p",
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAhcP9INehHWSK8ML7ONJaWefGjtrObCdQw3DbXiuZ016TRUw7"
-                            )
-                        ),array(
-                           'id'=> 8,
-                            "event_name" => "hello  Event Eight",
-                            "desc" => "hi hello",
-                            "event_date"=> "29th feb 2016 to 30th feb 2017",
-                            "year"=> "2017",
-                            "event_images" => array(
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvg1kO4EfJf6kyCiDeA-PRp-XNwg8DqsucY_OrkHjXqKd2HB2p",
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAhcP9INehHWSK8ML7ONJaWefGjtrObCdQw3DbXiuZ016TRUw7"
-                            )
-                        ),array(
-                           'id'=> 9,
-                            "event_name" => "hello  Event Nine",
-                            "desc" => "hi hello",
-                            "event_date"=> "29th feb 2016 to 30th feb 2017",
-                            "year"=> "2017",
-                            "event_images" => array(
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvg1kO4EfJf6kyCiDeA-PRp-XNwg8DqsucY_OrkHjXqKd2HB2p",
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAhcP9INehHWSK8ML7ONJaWefGjtrObCdQw3DbXiuZ016TRUw7"
-                            )
-                        ),array(
-                           'id'=> 10,
-                            "event_name" => "hello  Event Ten",
-                            "desc" => "hi hello",
-                            "event_date"=> "29th feb 2016 to 30th feb 2017",
-                            "year"=> "2017",
-                            "event_images" => array(
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvg1kO4EfJf6kyCiDeA-PRp-XNwg8DqsucY_OrkHjXqKd2HB2p",
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAhcP9INehHWSK8ML7ONJaWefGjtrObCdQw3DbXiuZ016TRUw7"
-                            )
-                        ),array(
-                           'id'=> 11,
-                            "event_name" => "hello  Event Eleven",
-                            "desc" => "hi hello",
-                            "event_date"=> "29th feb 2016 to 30th feb 2017",
-                            "year"=> "2017",
-                            "event_images" => array(
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvg1kO4EfJf6kyCiDeA-PRp-XNwg8DqsucY_OrkHjXqKd2HB2p",
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAhcP9INehHWSK8ML7ONJaWefGjtrObCdQw3DbXiuZ016TRUw7"
-                            )
-                        ),array(
-                           'id'=> 12,
-                            "event_name" => "hello  Event Tweleve",
-                            "desc" => "hi hello",
-                            "event_date"=> "29th feb 2016 to 30th feb 2017",
-                            "year"=> "2017",
-                            "event_images" => array(
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvg1kO4EfJf6kyCiDeA-PRp-XNwg8DqsucY_OrkHjXqKd2HB2p",
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAhcP9INehHWSK8ML7ONJaWefGjtrObCdQw3DbXiuZ016TRUw7"
-                            )
-                        ),array(
-                           'id'=> 13,
-                            "event_name" => "hello  Event Thirteen",
-                            "desc" => "hi hello",
-                            "event_date"=> "29th feb 2016 to 30th feb 2017",
-                            "year"=> "2017",
-                            "event_images" => array(
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvg1kO4EfJf6kyCiDeA-PRp-XNwg8DqsucY_OrkHjXqKd2HB2p",
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAhcP9INehHWSK8ML7ONJaWefGjtrObCdQw3DbXiuZ016TRUw7"
-                            )
-                        ),array(
-                           'id'=> 14,
-                            "event_name" => "hello  Event Fourteen",
-                            "desc" => "hi hello",
-                            "event_date"=> "29th feb 2016 to 30th feb 2017",
-                            "year"=> "2017",
-                            "event_images" => array(
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvg1kO4EfJf6kyCiDeA-PRp-XNwg8DqsucY_OrkHjXqKd2HB2p",
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAhcP9INehHWSK8ML7ONJaWefGjtrObCdQw3DbXiuZ016TRUw7"
-                            )
-                        ),array(
-                           'id'=> 15,
-                            "event_name" => "hello  Event Fifteen",
-                            "desc" => "hi hello",
-                            "event_date"=> "29th feb 2016 to 30th feb 2017",
-                            "year"=> "2017",
-                            "event_images" => array(
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvg1kO4EfJf6kyCiDeA-PRp-XNwg8DqsucY_OrkHjXqKd2HB2p",
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAhcP9INehHWSK8ML7ONJaWefGjtrObCdQw3DbXiuZ016TRUw7"
-                            )
-                        )
-                      );
-            $message = "Success";
-            $status = 200;
+             $data = array();
+             if ($request->has('year')) {
+                 $year = $request->year;
+             } else {
+                 $year = date("Y");
+             }
+             $ids = Events::whereYear('created_at', '=', $year)->pluck('id')->toArray();
+
+             if (count($ids) > 0) {
+                 $eventData = Events::orderBy('id', 'ASC')
+                     ->get()->toArray(); //all city data
+             } else {
+                 $eventData = Events::orderBy('id', 'ASC')
+                     ->whereIn('id', $ids)
+                     ->get()->toArray(); //all city data
+             }
+             $count = 0;
+             foreach ($eventData as $event) {
+                 if ($request->has('language_id')) {
+                     $eventTranslationData = EventsTranslations::where('language_id', $request->language_id)
+                         ->where('event_id', $event['id'])
+                         ->get()->toArray();
+                     if (count($eventTranslationData) > 0) {
+                         $data[] = array(
+                             'id' => $event['id'],
+                             'event_name' => $eventTranslationData[0]['event_name'],
+                             'desc' => $eventTranslationData[0]['description'],
+                             'venue' => $eventTranslationData[0]['venue'],
+                             'city' => Cities::where('id', $event['city_id'])->value('name'),
+                             'event_date' => date('d M y',strtotime($event['start_date']))." to ".date('d M y',strtotime($event['end_date'])),
+                             'year' => date('Y',strtotime($event['start_date'])),
+                         );
+                     } else {
+                         $data[] = array(
+                             'id' => $event['id'],
+                             'event_name' => $event['event_name'],
+                             'desc' => $event['description'],
+                             'venue' => $event['venue'],
+                             'city' => Cities::where('id', $event['city_id'])->value('name'),
+                             'event_date' => date('d M y',strtotime($event['start_date']))." to ".date('d M y',strtotime($event['end_date'])),
+                             'year' => date('Y',strtotime($event['start_date'])),
+                         );
+                     }
+
+                 } else {
+                     $data[] = array(
+                         'id' => $event['id'],
+                         'event_name' => $event['event_name'],
+                         'desc' => $event['description'],
+                         'venue' => $event['venue'],
+                         'city' => Cities::where('id', $event['city_id'])->value('name'),
+                         'event_date' => date('d M y',strtotime($event['start_date']))." to ".date('d M y',strtotime($event['end_date'])),
+                         'year' => date('Y',strtotime($event['start_date'])),
+                     );
+                 }
+
+                 $EventImgData = array();
+                 $eventImageData = EventImages::where('event_id', $event['id'])
+                     ->orderBy('id', 'ASC')
+                     ->get()->toArray();
+                 foreach ($eventImageData as $eventImage) {
+                     $evnImg = null;
+                     $createEventDirectoryName = sha1($event['id']);
+                     $evnImg = env('SGKSWEB_BASEURL') . env('EVENT_IMAGES_UPLOAD') . DIRECTORY_SEPARATOR . $createEventDirectoryName . DIRECTORY_SEPARATOR . $eventImage['url'];
+                     $EventImgData[] = $evnImg;
+                 }
+                 $data[$count]['event_images'] = $EventImgData;
+                 $count++;
+             }
+
+             $message = "Success";
+             $status = 200;
         }catch(\Exception $e){
-            $message = "Fail";
-            $status = 500;
-            $data = [
-                'action' => 'Get all members',
-                'exception' => $e->getMessage(),
-                'params' => $request->all()
-            ];
-            Log::critical(json_encode($data));
-        }
+             $message = "Fail";
+             $status = 500;
+             $data = [
+                 'action' => 'Get all Event data',
+                 'exception' => $e->getMessage(),
+                 'params' => $request->all()
+             ];
+             Log::critical(json_encode($data));
+         }
         $response = [
             'message' => $message,
             'data' => $data,
-            'page_id' => $page_id
         ];
         return response()->json($response,$status);
     }
